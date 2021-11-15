0a489292f1f2f674c978b7e44d40c96d62a45289
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2021-11-14 22:59:19 -0600

    Update settings.py

--- Assign3CT/settings.py
+++ Assign3CT/settings.py
@@ -123,7 +123,7 @@ LOGOUT_REDIRECT_URL = "home"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-
+STATIC_ROOT = os.path.join(BASE_DIR, '/')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 # Default primary key field type
