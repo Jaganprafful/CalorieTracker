038c79b4dd46eee02bb4d66068e3bf91810695d8
Jaganprafful <67191031+Jaganprafful@users.noreply.github.com>
2021-11-15 02:19:49 -0600

    Removed static root path

--- Assign3CT/settings.py
+++ Assign3CT/settings.py
@@ -123,7 +123,6 @@ LOGOUT_REDIRECT_URL = "home"
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, '/static')
 STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static/css')]
 
 # Default primary key field type
